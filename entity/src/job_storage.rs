56ee34fd761a93e9756ab2fa272dc265fb067885
whl2606555 <17124941+whl2606555@users.noreply.github.com>
2023-03-30 01:32:17 +0800

    fix: try to fix `mysql` issue by updating `sea-orm` version fix: remove `auto_increment` for uuid primary_key

--- entity/src/job_storage.rs
+++ entity/src/job_storage.rs
@@ -3,7 +3,7 @@ use sea_orm::entity::prelude::*;
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
 #[sea_orm(table_name = "job_storage")]
 pub struct Model {
-    #[sea_orm(primary_key)]
+    #[sea_orm(primary_key, auto_increment = false)]
     pub id: Uuid,
     pub task_name: String,
     pub cron: String,
